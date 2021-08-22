67b49452d6e4242d4d4cf5766527407faab48735
Timiajayi <61969771+timiajayi@users.noreply.github.com>
2021-08-22 10:59:00 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,96 @@
+<?php
+include ('validate.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>stage 2 task</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend.css">
+    <link rel="stylesheet" href="font-awesome/css/all.min.css">
+
+    <link rel="stylesheet" href="animate/animate.css">
+
+
+</head>
+
+    <body>
+    <div class="section1">
+                  
+                  <nav class="navbar navbar-expand-lg navbar-dark">
+                      <a class="navbar-brand" href="#">TWW <sub>timi world wide</sub> </a>
+                      
+                      <div class="navbar" id="navbarNav">
+                        <ul class="navbar">
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/ZURI.svg" alt="ZURI"width="100px" height="50px" ></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/HNG.png" alt="HNG"width="150px" height="50px"> </a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/I4G.png" alt="I4G" width="100px" height="50px"> </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </nav>
+                              
+              </div>
+
+        <p><br></p>
+        <div class="section2">
+        <div class="row">
+
+  <div class="column">
+    <div class="card">
+    <img class="card-img-top" src="img/IMG_1017.jpg" alt="timi ajayi">
+              
+                <h5 class="card-title">ajayi oluwatimilehin sunday</h5>
+               
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <p>
+    Welcome <?php echo $_POST["name"]; ?><br>
+With email: <?php echo $_POST["email"]; ?>
+  </p>          
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+    <h5 class="card-title">Backend stage 2 Task</h5>
+    <p class="card-text">click  below to access my resume (to be updated)</p>
+    <a href="#" class="btn btn-primary" onclick="window.open('resume.pdf', '_blank', 'fullscreen=yes'); return false;">My resume</a>
+
+    </div>
+  </div>
+</div>
+        </div>              
+       
+<p><br></p>
+
+<div class="section3">
+<div class="card text-center">
+    <div class="card-body">
+    
+  </div>
+  </div>
+       </div>
+<div class="footer">
+
+<p>@timiajayi 
+  
+</p>
+</div>
+
+
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
+  
---
+++ index.php
@@ -0,0 +1,195 @@
+<?php 
+include('validate.php'); 
+?>
+<!DOCTYPE html>
+<head>
+    <title>stage 2 task</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend.css">
+    <link rel="stylesheet" href="font-awesome/css/all.min.css">
+
+    <link rel="stylesheet" href="animate/animate.css">
+
+
+</head>
+
+    <body>
+    <div class="section1">
+                  
+                  <nav class="navbar navbar-expand-lg navbar-dark">
+                      <a class="navbar-brand" href="#">TWW <sub>timi world wide</sub> </a>
+                      
+                      <div class="navbar" id="navbarNav">
+                        <ul class="navbar">
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/ZURI.svg" alt="ZURI"width="100px" height="50px" ></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/HNG.png" alt="HNG"width="150px" height="50px"> </a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link bg-light" href="#"><img src="img/I4G.png" alt="I4G" width="100px" height="50px"> </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </nav>
+                              
+              </div>
+
+        <p><br></p>
+        <div class="section2">
+        <div class="row">
+
+  <div class="column">
+    <div class="card">
+    <img class="card-img-top" src="img/IMG_1017.jpg" alt="timi ajayi">
+              
+    <h5 class="card-title">ajayi oluwatimilehin sunday</h5>
+    <div class="header-social mb-3 d-print-none" data-aos="zoom-in" data-aos-delay="200">
+                  <nav role="navigation">
+                    <ul class="nav justify-content-center">
+                    
+                      <li class="nav-item"><a class="nav-link"target="_blank" href="https://www.facebook.com/timiajayi" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+                      </li>
+                      <li class="nav-item"><a class="nav-link" target="_blank" href="https://www.instagram.com/twwcreate" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+                      </li>
+                      <li class="nav-item"><a class="nav-link" target="_blank" href="https://github.com/timiajayi" title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>         
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+  
+    <h5 class="card-title" >ABOUT ME</h5>
+                <p class="card-text">Hello!.
+                   I am passionate Web development.
+                    I am a skilled front-end developer and good with graphic design tools such as Photoshop and adobe illustrator.<br><br>
+                  I am a quick learner and a team worker that gets the job done.
+
+    </div>
+  </div>
+  
+ 
+</div>
+        </div>              
+       
+<p><br></p>
+<div class="section3">
+<div class="shadow-1-strong bg-white my-5 p-5" id="skills">
+          <div class="skills-section">
+            <h2 class="h2 fw-light mb-4">Professional Skills</h2>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="mb-3"><span class="fw-bolder">HTML</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="100" data-aos-anchor=".skills-section" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">Master</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">CSS</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="200" data-aos-anchor=".skills-section" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">Expert</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">JavaScript</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 55%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">WordPress</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-info" role="progressbar" data-aos="zoom-in-right" data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 45%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Advance</div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3"><span class="fw-bolder">Adobe Photoshop</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 55%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">Master</div>
+                  </div>
+                </div>
+                <div class="mb-3"><span class="fw-bolder">Adobe Illustrator</span>
+                  <div class="progress my-2 rounded" style="height: 20px">
+                    <div class="progress-bar bg-secondary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 65%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">Expert</div>
+                  </div>
+                </div>
+                
+                
+              </div>
+            </div>
+          </div>
+        </div>
+</div>
+
+<p><br></p>
+<div class="section4">
+<div class="shadow-1-strong bg-white my-5 p-5" id="skills">
+<h5 class="card-title">Backend stage 2 Task</h5>
+    <p class="card-text">fill the formbelow to access my resume (to be updated)</p>
+
+    
+   
+    <form method="post" action="form.php" >  
+  Name: 
+  <input type="text" name="name" placeholder="name" required>
+  <span class="error"><?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: 
+  <input type="text" name="email" placeholder="email" required>
+  <span class="error"><?php echo $emailErr;?></span>
+  <br><br>
+  Website:
+   <input type="text" name="website" placeholder="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: 
+  <textarea name="comment" placeholder="comments" rows="5" ><?php echo $comment;?></textarea>
+  <br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+if (isset($_POST['submit']) && $error == '') {
+echo "<p class='success'>Form has been submitted successfully.</p>"; 
+
+$name=$_POST['name']; 
+$email=$_POST['email']; 
+$website=$_POST['website']; 
+$comments=$_POST['comments']; 
+
+$to="timiajayi2001@gmail.com";
+$subject="Requested your resume";
+$from="stage2 HNG backend";
+
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers  .= "From: $from\r\n"; 
+@mail($to, $subject, $message, $headers);
+// echo '<meta http-equiv="refresh" content="0;URL=form.php" />';
+}
+?>
+          
+        </div>
+</div>
+<p><br></p>
+
+
+
+<div class="footer">
+
+<p>@timiajayi 
+  
+</p>
+</div>
+
+
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
+  
